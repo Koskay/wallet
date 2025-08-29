0d3c44d7652c8fb7f087cf9871cbf9e857650a86
Kostya Spynaki <kostyaspynaki@Mac-mini-Kostya.local>
2025-08-29 19:18:13 +0300

    create wallet app

--- backend/core/apps/wallets/models.py
+++ backend/core/apps/wallets/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from django.db import models
+from core.apps.common.models import TimedBaseModel
+
+
+
+class Wallet(TimedBaseModel):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        verbose_name='Идентификатор'
+    )
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_wallet',
+        verbose_name='Пользователь'
+    )
+
+    balance = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=0.00,
+        validators=[MinValueValidator(0)], #  в теории, баланс может быть отрицательным, тут зависит от условий бизнеса
+        verbose_name='Баланс'
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='Статус кошелька'
+    )
+
+    class Meta:
+        verbose_name = 'Кошелек'
+        verbose_name_plural = 'Кошельки'
+
+
